c1edc3f180e6d0923d106e4f088508f7359fe2d6
Josip-github <josipcota1@gmail.com>
2021-10-15 16:29:04 +0200

    this commit adds new controller method greeting() The @GetMapping annotation makes sure that HTTP GET requests to endpoint "/greeting" are mapped to this method, i.e. greeting(). @RequestParam binds the value of the query string parameter name into the name parameter of the greeting() method. required=false means this queryy string parameter is not required. If it is absent in the request, default value will be used. That's why defaultValue="World". Method parameter String name is added to the model by model.addAttribute(...).

--- src/main/java/com/example/thymeleafdemo/controller/HomeController.java
+++ src/main/java/com/example/thymeleafdemo/controller/HomeController.java
@@ -2,14 +2,24 @@ package com.example.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Instant;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model){
-        model.addAttribute("welcomeMessage", "Hi Hello");
+        model.addAttribute("welcomeMessage", Instant.now().toString());
         return "home";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
