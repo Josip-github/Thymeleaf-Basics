85197213fea88fa2403b01f4dbbd0402152ba04d
Josip-github <josipcota1@gmail.com>
2021-10-16 09:05:22 +0200

    This commit adds a post method The goal of this commit is to display one message when the user visits the web-site for the first time, and when he submits the "visit me" button, another message is displayed.

--- src/main/java/com/example/thymeleafdemo/controller/HomeController.java
+++ src/main/java/com/example/thymeleafdemo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +14,7 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model){
-        model.addAttribute("greetings", new String[]{"Hi", "Hello", "goodbye"});
+        model.addAttribute("greetings", new String[]{"Hi", "Hello", "goodbye", "aaagoodbye", "Good day", "Goodbye"});
         return "home";
     }
 
@@ -22,5 +23,17 @@ public class HomeController {
         model.addAttribute("name", name);
         return "greeting";
         // If I want a name "John" to be displayed instead of "World", URL has to be: http://localhost:8080/greeting?name=John
+    }
+
+    @GetMapping("/simple-home")
+    public String getSimpleHome(Model model){
+        model.addAttribute("firstVisit", "Hello, Homepage!");
+        return "simple-home";
+    }
+
+    @PostMapping("/simple-home")
+    public String postSimpleHome(){
+        return "simple-home";
+
     }
 }
